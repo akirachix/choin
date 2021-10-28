f071b62474bbebefd41d477b1c4b85a89df7c0fa
sindetisarah <sindetisara@gmail.com>
2021-10-28 10:18:39 +0300

    create trainer_transaction_layout

--- trainer/urls.py
+++ trainer/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import trainer_dashboard
+from . views import trainer_dashboard,trainer_transcation
 urlpatterns =[
-    path('trainer_dashboard/',trainer_dashboard,name='trainer_dashboard')
+    path('trainer_dashboard/',trainer_dashboard,name='trainer_dashboard'),
+    path('trainer_transaction/',trainer_transcation,name="transaction.html")
 
     ]
--- trainer/views.py
+++ trainer/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def trainer_dashboard(request):
     return render(request,'trainer.html')
+    
+def trainer_transcation(request):
+    return render(request,'transaction.html')
