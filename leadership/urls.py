8ca4c4663666c26908de40e0b859a6d012971afd
chantal2000 <nniyonkuruchantal@gmail.com>
2021-11-10 11:20:51 +0300

    add search a student by Admin

--- leadership/urls.py
+++ leadership/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import add_reward, ajax_change_status, deactivate_ajax_change_status,search_redeemable_item, add_transaction,search_student,connect_node, delete_metric, edit_metric, get_chain, is_valid, profile_upload, replace_chain, reward, reward_confirm,trainer_profile_upload,addMetric, trans, redeemableItemsList
+from . views import add_reward, ajax_change_status,search_student_admin, deactivate_ajax_change_status,search_redeemable_item, add_transaction,search_student,connect_node, delete_metric, edit_metric, get_chain, is_valid, profile_upload, replace_chain, reward, reward_confirm,trainer_profile_upload,addMetric, trans, redeemableItemsList
 urlpatterns =[
     path('get_chain/', get_chain, name="get_chain"),
     path('add_transaction/', add_transaction, name="add_transaction"), #New
@@ -19,6 +19,7 @@ urlpatterns =[
     path('redeemable-items/', redeemableItemsList, name='redeemable-items'),
     path('search_redeemable-items/',search_redeemable_item, name='search_redeemable_item'),
     path('ajax/change_status/', ajax_change_status, name='ajax_change_status'),
+    path('search_student_byadmin/',search_student_admin, name='search_student_admin'),
     path('deactivate/ajax/change_status/', deactivate_ajax_change_status, name='deactivate_ajax_change_status')
 
 
