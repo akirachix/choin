cbb2aa6eba1c2b5652419e7d24de9b9c8921896c
Eunice Atieno <eunniceatieno@gmail.com>
2021-11-18 09:46:18 +0300

    final sprint

--- static/js/redeem.js
+++ static/js/redeem.js
@@ -29,7 +29,7 @@ function getToken(name) {
 var csrftoken = getToken('csrftoken');
 
 function updateUserOrder(productId, action) {
-    var url = "http://127.0.0.1:8000/student/update_item/"
+    var url = "https://choin.herokuapp.com/student/update_item/"  
     fetch(url, {
             method: 'POST',
             headers: {
